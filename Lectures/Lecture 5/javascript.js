0e67c1980854dcd51b4199587380dae717c84cde
eribrin <eribrinrin@gmail.com>
2024-02-05 14:26:06 -0500

    added class notes on js

---
+++ Lectures/Lecture 5/javascript.js
@@ -0,0 +1,20 @@
+/*
+Javascript
+- not related to java, just has a similar name
+- more closely related to python
+- loosely typed; nothing's gonna tell you if you access something that doesn't exist; you can make a variable for a string and put a
+  number in it and run it as an array, it'll just try to figure it out
+- interpretive language
+    - a compiled langiage has you write your code, then it goes through a compiler and puts out machine code
+    - an interpretive language means you ship your code to the consumer and it's run on that platform
+    - on a spectrum: a truly compiled language runs on the hardware; an interpretive language has you send the actual code and put it one
+      the computer you're running it on
+    - every time you run it your code gets fed into an interpreter
+    - uses a system called jit: just in time compiling; adds things to a cache and im confused
+    - you don't actually use your python code; you use your python code to execute commands that run in c and c++ and rust etc
+- javascript things
+    - methods don't belond to objects or classes, they're just stored in that field
+    - if you want to take that function and assign it to a variable, you can it's just living outside of everything
+    - the concept of this that exists in every other language doesn't exist in javascript
+    - you can totally redefine the keyword, cause functions don't actually belong to classes or object
+*/ 
