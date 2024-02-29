099c906e3c5621e36b4e69af3dacf47958c983d5
eribrin <eribrinrin@gmail.com>
2024-02-29 15:23:46 -0500

    added file based routing (need to fix this with his actual code because I missed a few steps)

--- project-examples/Client/src/router/index.ts
+++ project-examples/Client/src/router/index.ts
@@ -1,8 +1,12 @@
+// this determines what page goes where
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../pages/HomeView.vue'
 
+
 const router = createRouter({
+  // how do we determine what to display
   history: createWebHistory(import.meta.env.BASE_URL),
+  // the routes which is an array of objects, each has a path and optionally a name, and the component it shows
   routes: [
     {
       path: '/',
@@ -15,9 +19,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      // preload the ones that people will most likely go to earlier than others
+      // also consider how big a page is to preload it
       component: () => import('../pages/AboutView.vue')
     }
   ]
 })
 
+// this is the export to make it available
 export default router
