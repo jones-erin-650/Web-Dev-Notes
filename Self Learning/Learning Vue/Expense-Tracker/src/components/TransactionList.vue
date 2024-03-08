5cac3de65acb0fcb472ef63b98303fdee2804ec6
eribrin <eribrinrin@gmail.com>
2024-03-08 16:31:47 -0500

    added data to the transaction list component and a way to render them

--- Self Learning/Learning Vue/Expense-Tracker/src/components/TransactionList.vue
+++ Self Learning/Learning Vue/Expense-Tracker/src/components/TransactionList.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-        <h3>History</h3>
-        <ul id="list" class="list">
-            <li class="minus">
-                Cash <span>-$400</span>
-                <button class="delete-btn"></button>
-            </li>
-            <li class="plus">
-                Paycheck <span>$600</span>
-                <button class="delete-btn"></button>
-            </li>
-        </ul>
-    </div>
+    <h3>History</h3>
+    <ul id="list" class="list">
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class ="transaction.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ transaction.text }} <span>${{ transaction.amount }}</span
+        >
+      </li>
+    </ul>
   </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                transactions: [
+                { id: 1, text: 'mars soul', amount: -287.54},
+                { id: 2, text: 'idk', amount: 4543.34},
+
+                ]
+            }
+        }
+    }
+</script>
